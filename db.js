901aee819b8da4beda3114a36aff7dcd1413eae3
Kshitij <kshitij.paliya1@gmail.com>
2024-12-18 01:43:32 +0530

    Render7

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 import pg from "pg";
-const client = new pg.Client({
+const db = new pg.Client({
   host: "ep-super-lake-a147s83m.ap-southeast-1.aws.neon.tech",
   port: 5432,
   user: "vibeZ_owner",
@@ -8,8 +8,4 @@ const client = new pg.Client({
   ssl: { rejectUnauthorized: false }, // SSL for Neon DB
 });
 
-client
-  .connect()
-  .then(() => console.log("Connected to Neon DB!"))
-  .catch((err) => console.error("Error connecting to DB:", err));
 export default db;
